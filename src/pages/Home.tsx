8a7afef2472143483c4eebb0d1716f59ef779ff4
mahiramzade <Mahira54904077860096>
2022-01-23 07:21:48 +0400

    feat: add home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '../routes';
+
 export const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="absolute top-0 flex items-center justify-center w-full h-screen">
+      <div className="flex flex-col items-center">
+        <div className="flex mb-4">
+          <h1 className="text-6xl font-bold md:text-9xl">Examut</h1>
+          <span className="md:text-[12rem] text-9xl leading-3 text-primary md:leading-[84px]">
+            .
+          </span>
+        </div>
+
+        <Link to={ROUTES.login}>
+          <Button
+            size="large"
+            color="primary"
+            variant="contained"
+            className="font-bold"
+            disableElevation
+          >
+            Get Started
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
